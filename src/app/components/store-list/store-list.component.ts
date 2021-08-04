faa3ecd09c5b38a191bb62af4b6b7ba5a4a8ff92
LAPTOP-1CACDRFD\Hossam Mamdouh <hossammamdouh8010@gmail.com>
2021-08-04 19:16:21 +0200

    get all stores in home page

---
+++ src/app/components/store-list/store-list.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { StoreService } from 'src/app/services/store.service';
+import { ActivatedRoute , Router } from '@angular/router';
+
+@Component({
+  selector: 'app-stores-list',
+  templateUrl: './store-list.component.html',
+  styleUrls: ['./store-list.component.css']
+})
+export class StoresListComponent implements OnInit {
+
+  stores: any;
+  currentStore = null;
+  currentIndex = -1;
+  storeName = '';
+
+  constructor(private StoreService: StoreService,private router: Router, private route: ActivatedRoute,) { }
+
+  ngOnInit(): void {
+    this.retrieveStores();
+  }
+
+  retrieveStores(): void {
+    this.StoreService.getAll()
+      .subscribe(
+        data => {
+          this.stores = data.data ;
+          console.log(data.data);
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  refreshList(): void {
+    this.retrieveStores();
+    this.currentStore = null;
+    this.currentIndex = -1;
+  }
+
+  setActiveStore(store, index): void {
+    this.currentStore = store;
+    this.currentIndex = index;
+  }
+}
